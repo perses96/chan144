3753f9d29a950bfeefdf21c730decd217fd5ab98
pablo <imageofmyself@protonmail.com>
2018-01-12 12:44:01 -0600

    www

--- chan144/settings.py
+++ chan144/settings.py
@@ -119,9 +119,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MIDDLEWARE_CLASSES = [
-  # 'django.middleware.security.SecurityMiddleware',
-  'whitenoise.middleware.WhiteNoiseMiddleware',
-  # ...
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
