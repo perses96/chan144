b8b50f696e89329fb2a577c47662ffb2a8209526
pablo <imageofmyself@protonmail.com>
2018-01-22 17:50:20 -0600

    now

--- chan144/urls.py
+++ chan144/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'',include("welcome.urls"))
+    path(r'1',include("welcome.urls"))
 ]
